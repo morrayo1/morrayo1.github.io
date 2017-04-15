0b8bbaa7404aec94c0fd1dc76894e3c5f1ec5c73
akswnd98 <mjk4509@naver.com>
2017-04-15 10:54:05 +0900

    박스박스 커밋커밋

--- js/script.js
+++ js/script.js
@@ -1,13 +1,5 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: '안녕하세요 Vue!'
-  }
-})
-
-var app2 = new Vue({
-  el: '#app-2',
-  data: {
-    message: '이 페이지는 ' + new Date() + ' 에 로드 되었습니다'
-  }
-})
+$(document).ready(function(){
+    $('box').on('click',function(){
+        alert('박스박스');
+    });
+});
